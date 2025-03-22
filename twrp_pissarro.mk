8272c21fdd026d98a2dffebb0f9adad7d9930ac2
ShazuxD <shazuxd7@gmail.com>
2025-03-22 22:55:00 +0530

    pissarro: adapt to twrp
    Signed-off-by: shazuxd <shazuxd7@gmail.com>

--- twrp_pissarro.mk
+++ twrp_pissarro.mk
@@ -9,21 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from pissarro device
 $(call inherit-product, device/xiaomi/pissarro/device.mk)
 
 PRODUCT_DEVICE := pissarro
-PRODUCT_NAME := omni_pissarro
-PRODUCT_BRAND := Redmi
+PRODUCT_NAME := twrp_pissarro
+PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := pissarro
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_pissarro-user 12 SP1A.210812.016 V816.0.5.0.TKTMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_pissarro/pissarro:12/SP1A.210812.016/V816.0.5.0.TKTMIXM:user/release-keys
